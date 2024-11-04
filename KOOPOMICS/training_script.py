69650ee925fb06f4d3bd30e03e57b9126e0c7daa
Philipp Trinh <trinh@login01.lisc>
2024-11-04 12:22:44 +0100

    embedding training implemented
    Former-commit-id: d81253da90a0e67c0ecc5a18025a6d72a2ef8547

--- KOOPOMICS/training_script.py
+++ KOOPOMICS/training_script.py
@@ -16,16 +16,16 @@ train_set_df = pregnancy_df[pregnancy_df['Cohort'] == 'Discovery'].copy()
 test_set_df = pregnancy_df[pregnancy_df['Cohort'] == 'Validation (Test Set 1)'].copy()
 
 train_dataloader = ko.OmicsDataloader(train_set_df, feature_list, replicate_id, 
-                                      batch_size=5, max_Ksteps = 1)
+                                      batch_size=10, max_Ksteps = 0)
 test_dataloader = ko.OmicsDataloader(test_set_df, feature_list, replicate_id,
-                                     batch_size=5, max_Ksteps = 1)
+                                     batch_size=10, max_Ksteps = 0)
 
 runconfig = ko.RunConfig()
 runconfig.num_metabolites = 50
 runconfig.feature_selected=True
 
 # Load Model
-embedding_model = ko.FF_AE([50,1000,1000,20], [20,1000,1000,50],E_dropout_rates= [0,0.2,0,0],activation_fn='leaky_relu')
+embedding_model = ko.FF_AE([50,2000,20], [20,2000,50],E_dropout_rates= [0,0.3,0],activation_fn='leaky_relu')
 operator_model = ko.InvKoop(latent_dim=20, reg=None,activation_fn='leaky_relu')
 
 TestingKoopnondelay = ko.KoopmanModel(embedding=embedding_model, operator=operator_model)
@@ -33,9 +33,9 @@ baseline = ko.NaiveMeanPredictor(train_set_df, feature_list, mask_value=-2)
 
 # Run training loop
 TestingKoopnondelay.fit_embedding(train_dataloader, test_dataloader, runconfig=runconfig,
-                         num_epochs = 100, lr=0.001, max_Kstep=1,
+                         num_epochs = 100, lr=0.00001, max_Kstep=0,
                          loss_weights = [1,0.5,1,1,0.01,0], mask_value=-2,
-                         model_name = 'TestingKoopNonDelay_lrelu_M264', use_wandb=True,
-                        learning_rate_change=0.2,
+                         model_name = 'TestingKoopNonDelay_lrelu_M50', use_wandb=True,
+                        learning_rate_change=0.8,
                         decayEpochs=[30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 410, 440, 470, 500],
                          baseline=baseline)
